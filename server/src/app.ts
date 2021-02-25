a509458a992c4811e55e90acc46a88aae3557079
kacus <54528490+kacus@users.noreply.github.com>
2021-02-25 13:16:01 +0100

    Fix configuration and clean up dist
    Config fixes:
    Environment
    - Delete nodemon.json file and use 'dotenv' instead. See .env-pattern file to get list of required variables.
    - Add '.env' file to .gitignore for security reasons.
    Dist folder
    - Delete 'dist' folder and add it to .gitignore
    Dependencies
    - Delete "@types/mongoose" because Mongoose includes TypeScript types since mongoose@5.11.0
    - Delete "concurrently"
    - Add "dotenv"
    - Add "ts-node", so there is no more need for user to have it globally
    - Update version of "mongoose"
    Scripts
    - Add 'dev' script to run dev server
    - Update 'start' script to run production version of code. Requires a built project.
    Project name
    Renamed to a more appropriate name

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,12 +3,16 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 
 const app: Express = express()
+require('dotenv').config()
+
 
 const PORT: string | number = process.env.PORT || 4000
-
+const MONGO_USER: string = process.env.MONGO_USER || 'CC-Forum-App';
+const MONGO_PASSWORD: string = process.env.MONGO_PASSWORD || 'bad-pass';
+const MONGO_DB: string = process.env.MONGO_DB || 'CC-Forum';
 app.use(cors())
 
-const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@clustertodo.raz9g.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+const uri: string = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@cc-forum.4k1nv.mongodb.net/${MONGO_DB}?retryWrites=true&w=majority`
 const options = { useNewUrlParser: true, useUnifiedTopology: true }
 mongoose.set('useFindAndModify', false)
 
