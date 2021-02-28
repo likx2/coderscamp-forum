bea5c1b2a5315ea6ccccf93625f42a6f3d716a09
kacus <54528490+kacus@users.noreply.github.com>
2021-02-28 13:50:36 +0100

    Rename route for users

--- server/src/startup/routes.ts
+++ server/src/startup/routes.ts
@@ -2,7 +2,7 @@ import express, { Express } from 'express'
 import cors from 'cors'
 import errorHandler from '../middleware/errorHandler'
 
-import authRouter from '../routes/auth'
+import users from '../routes/users'
 
 //Handle async errors in request pipeline
 require('express-async-errors')
@@ -11,7 +11,7 @@ export default function startupRoutes(app: Express){
     app.use(cors())
     app.use(express.json())
 
-    app.use('/', authRouter)
+    app.use('/users', users)
 
     //error handling middleware in request pipeline
     app.use(errorHandler)
