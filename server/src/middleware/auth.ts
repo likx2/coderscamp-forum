8f756b52e547c573afeb32498263331281f5bb9a
kacus <54528490+kacus@users.noreply.github.com>
2021-03-02 00:00:26 +0100

    Use callback instead try-catch block to verify token

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -1,24 +1,26 @@
 import jwt from 'jsonwebtoken'
-import {Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 import IAuthTokenPayload from '../types/IAuthTokenPayload';
 import IAuthenticatedRequest from '../types/IAuthenticatedRequest';
 
-export default function auth(req: IAuthenticatedRequest, res: Response, next: NextFunction){
+export default function auth(req: IAuthenticatedRequest, res: Response, next: NextFunction) {
     //Read auth token from request
     const authToken = req.header('x-auth-token')
-    if(!authToken){
+    if (!authToken) {
         return res.status(401).send('Access denied. No auth token provided.')
     }
 
-    try{
+    jwt.verify(authToken, process.env.JWT_PRIVATE_KEY!, (err, decoded) => {
+        if (err) {
+            return res.status(400).send('Invalid auth token provided.')
+        }
+
         //Decode provided token
         //generateAuthToken always should use IAuthTokenPayload to create payload so this typing should be save
-        const decodedPayload = jwt.verify(authToken, process.env.JWT_PRIVATE_KEY!) as IAuthTokenPayload
-        
+        const decodedPayload = decoded as IAuthTokenPayload
+
         //Forward decoded user in request
         req.user = decodedPayload
         next();
-    }catch(ex){
-        return res.status(400).send('Invalid auth token provided.')
-    }
+    })
 }
