3821f7785335c8cbd676d459591dc9344d05d3bd
kacus <54528490+kacus@users.noreply.github.com>
2021-03-04 21:43:56 +0100

    Fix for errorHandler
    Now express can use this handler as an error handler.
    
    According to docs.:
    
    Error-handling middleware always takes four arguments. You must provide four arguments to identify it as an error-handling middleware function. Even if you don’t need to use the next object, you must specify it to maintain the signature. Otherwise, the next object will be interpreted as regular middleware and will fail to handle errors.

--- server/src/middleware/errorHandler.ts
+++ server/src/middleware/errorHandler.ts
@@ -1,6 +1,12 @@
-import { Request, Response } from 'express'
+import express from 'express'
 
-export function errorHandler(err: Error, req: Request, res: Response): void {
-  console.error(err.message, err)
-  res.status(500).send('Something failed.')
+export function errorHandler(
+  err: Error,
+  req: express.Request,
+  res: express.Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  next: express.NextFunction,
+): void {
+  console.error(err)
+  res.status(500).send(err)
 }
