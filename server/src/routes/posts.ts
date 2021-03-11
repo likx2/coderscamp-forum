8084502e2420a331a5996afa61c0d7b363026eea
maruulv <marulson95@gmail.com>
2021-03-11 23:18:33 +0100

    koniec na dzis

--- server/src/routes/posts.ts
+++ server/src/routes/posts.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from 'express'
 import { Post, validateNewPost } from '../models/Post'
+import { Comment, validateNewComment } from '../models/Comment'
 import { auth } from '../middleware/auth'
 import { AuthenticatedRequest } from '../types/AuthenticatedRequest'
 
@@ -94,26 +95,55 @@ postRouter.get('/ranking/:hashtag', auth, async (req: AuthenticatedRequest, res)
   const sortingParameter = req.query.type ? req.query.type : 'reactions'
   const page = req.query.page ? parseInt(req.query.page as string) : 1
   const limit = req.query.limit ? parseInt(req.query.limit as string) : 10
-  let result =  await Post.aggregate([
-    {
-      '$project': {
-        'length': { '$size': '$hashtags' },
-      },
-    },
-    { '$sort': { 'length': -1 } },
-    { '$skip': page },
-    { '$limit': limit },
-  ]) 
-  res.status(200).send(result)
 
-  // await Post.find({ hashtags: hashtag })
-  //   .skip((page - 1) * limit)
-  //   .limit(limit)
-  //   .then((posts) => {
-  //     if (!posts) {
-  //       res.status(404).send('None posts found.')
-  //     } else {
-  //       res.status(200).send(posts)
-  //     }
-  //   })
+  const getSortedByComments = async (hashtag: string): Promise<any> => {}
+
+  const getSortedByReactions = async (hashtag: string): Promise<any> => {
+    return Post.aggregate([
+      {
+        '$project': {
+          'author': 1,
+          'title': 1,
+          'content': 1,
+          'imageUrl': 1,
+          'hashtags': 1,
+          'reactions': 1,
+          'length': { '$size': `$reactions` },
+          'containsHashtags': { '$in': [hashtag, '$hashtags'] },
+        },
+      },
+      { '$match': { 'containsHashtags': { '$eq': true } } },
+      { '$sort': { 'length': -1 } },
+      { '$skip': (page - 1) * limit },
+      { '$limit': limit },
+    ])
+  }
+})
+
+postRouter.post('/:id/comments', auth, async (req: AuthenticatedRequest, res: Response) => {
+  await Post.findById(req.params.id).then((post) => {
+    if(!post){
+      return res.status(404).send("Post not found")
+    }
+    const { error, value } = validateNewComment(req.body)
+    if (error) {
+      return res.status(400).send(error.details[0].message)
+    }
+
+    const newComment = new Comment({
+      ...value,
+      author: req.user?._id,
+      post: post._id,
+    })
+
+    newComment.save((err) => {
+      if (err) {
+        res.status(401).send(err)
+      } else {
+        res.status(201).send(newComment)
+      }
+    })
+  }).catch(err => {
+    return res.status(403).send("Wrong request.")
+  })
 })
