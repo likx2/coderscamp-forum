6ff83f02e58119e1f078becf5c8c5d11fa905d82
meloniak14 <w.melone97@gmail.com>
2021-04-02 09:20:59 +0100

    Connect with redux toolkit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,37 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import { ThemeProvider } from 'styled-components';
 
 import Layout from './components/Layout';
+import { getPosts, Post, postsSelector } from './store/PostSlice';
+// import {Post} from './store/PostSlice';
 import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/mainTheme';
 
-const App: FC = () => (
-  <>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Router>
-        <Layout>
-          <h1>posts</h1>
-          <h1>posts</h1>
-          <h1>posts</h1>
-        </Layout>
-      </Router>
-    </ThemeProvider>
-  </>
-);
+const App: FC = () => {
+  const dispatch = useDispatch();
+  const { posts, loading, errors } = useSelector(postsSelector);
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Router>
+          <Layout>
+            {posts.map((post: Post) => (
+              <p>{post.title}</p>
+            ))}
+          </Layout>
+        </Router>
+      </ThemeProvider>
+    </>
+  );
+};
 
 export default App;
